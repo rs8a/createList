afb8e63321bfbddf7bee068d722d23bcc9723209
Raul Sandoval <raulsandoval@macraul.local>
2020-12-24 13:38:43 -0600

    first commit

---
+++ makeList.py
@@ -0,0 +1,10 @@
+import os
+preffix = raw_input("ingrese el preffix:")
+result = []
+flist = os.listdir('.')
+for mfile in flist:
+    if mfile.endswith(".mp4"):
+        result.append(preffix+'/'+mfile)
+with open(preffix+".txt", "w") as txt_file:
+    for mfile in result:
+        txt_file.write("".join(mfile) + "\n")
